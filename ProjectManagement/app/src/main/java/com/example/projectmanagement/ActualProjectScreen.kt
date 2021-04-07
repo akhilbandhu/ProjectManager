b91a990af82d013c29173b75a4527f90076682a1
DeAwesome <favoritechild7@gmail.com>
2021-04-07 17:16:14 -0600

    Screen Designs

---
+++ ProjectManagement/app/src/main/java/com/example/projectmanagement/ActualProjectScreen.kt
@@ -0,0 +1,11 @@
+package com.example.projectmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActualProjectScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actual_project_screen)
+    }
+}
--- ProjectManagement/app/src/main/java/com/example/projectmanagement/MainActivity.kt
+++ ProjectManagement/app/src/main/java/com/example/projectmanagement/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.example.useraccounts
+package com.example.projectmanagement
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.projectmanagement.R
+import com.example.projectmanagementdeanna.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
